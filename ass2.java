e24a22d145c79afc39000fb04d3d12413083ae10
Surajnade <108931269+Surajnade@users.noreply.github.com>
2022-07-08 16:41:26 +0530

    Add files via upload

---
+++ ass2.java
@@ -0,0 +1,12 @@
+class ass2
+{
+      public static void main(String []args)
+      {
+         int i, no=24;
+         for(i=1;i<=24;i++)
+         {
+            if(no%i==0)
+             System.out.println( no + " number is divisible by " + i );
+         }
+      }
+}
